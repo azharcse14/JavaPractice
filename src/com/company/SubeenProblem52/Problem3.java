090a0f0a85ef729f1fdcb0b8a078fab159988ca3
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2020-07-03 17:09:28 +0600

    Problem Solving
    Problem Solving from 52 programming problem by Tamim Sahriar Subben.

---
+++ src/com/company/SubeenProblem52/Problem3.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        int i, n=1000;
+        int j=1;
+
+
+        for (i=n;i>=1;i--,j++){
+
+            if(i<10){
+                System.out.print(i+"   "+"\t");
+            }else if(i<100){
+                System.out.print(i+"  "+"\t");
+            }else if(i<1000){
+                System.out.print(i+" "+"\t");
+            }else {
+                System.out.print(i+"\t");
+            }
+
+
+
+            if (j%5==0 && i!=n){
+                System.out.println();
+            }
+
+        }
+    }
+
+}
---
+++ src/com/company/SubeenProblem52/Problem4.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
