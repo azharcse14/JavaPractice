49e679caf82ca78d54e78db59c4290f5d2ce6e17
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2020-07-04 12:23:47 +0600

    complete

--- src/com/azhar/array/SumOfLowerTriangleElementsOfaMatrix.java
+++ src/com/azhar/array/SumOfLowerTriangleElementsOfaMatrix.java
@@ -1,4 +1,28 @@
 package com.azhar.array;
 
+import java.util.Scanner;
+
 public class SumOfLowerTriangleElementsOfaMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int [][] A = new int[3][3];
+        int SumOfLowerTriangleElements = 0;
+
+        System.out.println("Input the Matrix: ");
+        for (int row = 0; row<3; row++){
+            for (int col =0; col<3; col++){
+                A[row][col]= scanner.nextInt();
+            }
+        }
+
+        for (int row = 0; row<3; row++){
+            for (int col =0; col<3; col++){
+                if (row<col){
+                    SumOfLowerTriangleElements = SumOfLowerTriangleElements + A[row][col];
+                }
+            }
+        }
+        System.out.println("Sum Of Lower Triangle Elements: "+SumOfLowerTriangleElements);
+    }
 }
