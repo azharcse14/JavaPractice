3cee9775316c9648a70784284583fdd0c452fe91
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2020-07-04 12:20:53 +0600

    complete

--- src/com/azhar/array/SumOfDiagonalElementsOfaMatrix.java
+++ src/com/azhar/array/SumOfDiagonalElementsOfaMatrix.java
@@ -6,6 +6,23 @@ public class SumOfDiagonalElementsOfaMatrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int [][] A = new int[3][3];
+        int SumOfDiagonalElements = 0;
 
+        System.out.println("Input the Matrix: ");
+        for (int row = 0; row<3; row++){
+            for (int col =0; col<3; col++){
+                A[row][col]= scanner.nextInt();
+            }
+        }
+
+        for (int row = 0; row<3; row++){
+            for (int col =0; col<3; col++){
+                if (row==col){
+                    SumOfDiagonalElements = SumOfDiagonalElements + A[row][col];
+                }
+            }
+        }
+        System.out.println("Sum Of Diagonal Elements: "+SumOfDiagonalElements);
     }
 }
