fdb1fd6d296d2e9d161a08271bf6bdd871487cc0
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2020-07-04 12:00:09 +0600

    new class

---
+++ src/com/company/array/SumOfDiagonalElementsOfaMatrix.java
@@ -0,0 +1,11 @@
+package com.company.array;
+
+import java.util.Scanner;
+
+public class SumOfDiagonalElementsOfaMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+
+    }
+}
